11e296c1029de7010473b82dcb3417abd5fdf646
Jeremy Hsu <jeremyhsu1@gmail.com>
2026-01-02 22:46:43 -0800

    Consider server load and capacity

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+DEFAULT_SERVER_CAPACITY = 5
 DEFAULT_NUM_SERVERS = 5
+
+
+class Server:
+
+    def __init__(self, id, capacity=0):
+        self.id = id
+        self.load = 0
+        if not capacity:
+            capacity = DEFAULT_SERVER_CAPACITY
+        self.capacity = capacity
 
 
 class RoundRobinLoadBalancer:
@@ -6,16 +17,20 @@ class RoundRobinLoadBalancer:
     def __init__(self, num_servers: int = 0):
         if not num_servers:
             num_servers = DEFAULT_NUM_SERVERS
-        self.servers = [i for i in range(num_servers)]
+        self.servers = [Server(i) for i in range(num_servers)]
         self.counter = 0
 
     """
-    handle_request returns the id of the server the request was routed to.
+    handle_request returns the the server the request was routed to. If the
+    target server is at capacity, handle_request returns -1.
     """
-    def handle_request(self) -> int:
+    def handle_request(self) -> Server:
         target = self.counter % len(self.servers)
         self.counter += 1
-        return target
+        if self.servers[target].load == self.servers[target].capacity:
+            return -1
+        self.servers[target].load += 1
+        return self.servers[target]
 
 
 def main():
@@ -26,7 +41,12 @@ def main():
     print('Routing {} requests...'.format(requests))
     for i in range(requests):
         targeted_server = round_robin_load_balancer.handle_request()
-        print('- R{} -> S{}'.format(i, targeted_server))
+        print('- R{} -> S{} (Load: {} Capacity: {})'.format(
+            i,
+            targeted_server.id,
+            targeted_server.load,
+            targeted_server.capacity,
+        ))
 
 
 if __name__ == '__main__':
