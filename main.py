083c8b87c1d119f739c789c36dc2891fa5a919e7
Jeremy Hsu <jeremyhsu1@gmail.com>
2026-01-03 01:18:53 -0800

    Rename handle_request to forward_request

--- main.py
+++ main.py
@@ -24,15 +24,15 @@ class RoundRobinLoadBalancer:
         self.counter = 0
 
     """
-    handle_request returns the the server the request was routed to. If the
-    target server is at capacity, handle_request returns -1.
+    forward_request returns the the server the request was routed to. If the
+    target server is at capacity, forward_request returns -1.
 
     The fundamental limitation of static routing algorithms such as round robin
     is that they are (without modifications) blissfully unaware of actual server
     state. In this case, this means that the request will be routed to the
     "next" server regardless of whether the server is overloaded or not.
     """
-    def handle_request(self) -> Server:
+    def forward_request(self) -> Server:
         target = self.counter % len(self.servers)
         self.counter += 1
         self.servers[target].load += 1
@@ -55,14 +55,14 @@ class WeightedRoundRobinLoadBalancer:
         self.seen_count_for_server = [0 for _ in range(num_servers)]
 
     """
-    handle_request forwards a request to a server, returning the server.
+    forward_request forwards a request to a server, returning the server.
 
     TODO: The current implementation is naive as it forwards all requests to a
     single server before it runs out of "space." We should implement the
     interleaved approach in order to smooth out request forwarding amongst
     servers over time.
     """
-    def handle_request(self) -> Server:
+    def forward_request(self) -> Server:
         target = -1
         while True:
             target = self.counter % len(self.servers)
@@ -85,7 +85,7 @@ def main():
     requests = 10
     print('Routing {} requests...'.format(requests))
     for i in range(requests):
-        targeted_server = round_robin_load_balancer.handle_request()
+        targeted_server = round_robin_load_balancer.forward_request()
         print('- R{} -> S{} (Load: {} Capacity: {})'.format(
             i,
             targeted_server.id,
@@ -98,7 +98,7 @@ def main():
     requests = 10
     print('Routing {} requests...'.format(requests))
     for i in range(requests):
-        targeted_server = weighted_round_robin_load_balancer.handle_request()
+        targeted_server = weighted_round_robin_load_balancer.forward_request()
         print('- R{} -> S{} (Load: {} Capacity: {})'.format(
             i,
             targeted_server.id,
