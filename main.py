1a627da56511bce2d94192ca4eb1e9643602f9d9
Jeremy Hsu <jeremyhsu1@gmail.com>
2026-01-02 19:13:46 -0800

    Route 10 requests

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ class RoundRobinLoadBalancer:
     def __init__(self, num_servers: int = 0):
         if not num_servers:
             num_servers = DEFAULT_NUM_SERVERS
-        self.servers = [i for i in range(len(num_servers))]
+        self.servers = [i for i in range(num_servers)]
         self.counter = 0
 
     """
     handle_request returns the id of the server the request was routed to.
     """
     def handle_request(self) -> int:
-        target = self.current_server % len(self.servers)
+        target = self.counter % len(self.servers)
         self.counter += 1
         return target
 
 
 def main():
-    print('Hello world!')
+    print('Initializing a round robin load balancer...')
+    round_robin_load_balancer = RoundRobinLoadBalancer()
+
+    requests = 10
+    print('Routing {} requests...'.format(requests))
+    for i in range(requests):
+        targeted_server = round_robin_load_balancer.handle_request()
+        print('- R{} -> S{}'.format(i, targeted_server))
 
 
 if __name__ == '__main__':
